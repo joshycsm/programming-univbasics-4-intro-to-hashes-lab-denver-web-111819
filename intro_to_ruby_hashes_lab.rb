eb334ac911c220adc2c746542973c160f1fea46f
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-03 10:34:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   # omg really!!
   {key: value}
+  my_hash_creator
   # my_hash_creator = {
   #   :key => value
   # }
